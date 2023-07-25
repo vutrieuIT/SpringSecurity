bfc287a4ea7b2214ac4f3d912ec6410adb81e358
Trieu Vu <vutrieu2002@gmail.com>
2023-07-25 15:25:11 +0700

    Spring Security: create User entity

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -17,10 +17,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(
+                        (csrf) -> csrf.disable()
+                )
                 .authorizeHttpRequests(
                         (requests) -> requests
-                        .requestMatchers( "/home").permitAll()
-                        .anyRequest().authenticated()
+                                .requestMatchers("/home").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .formLogin(
                         (form) -> form.permitAll()
