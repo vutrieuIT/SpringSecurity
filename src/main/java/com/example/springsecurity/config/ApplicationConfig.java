54a6a4cd11cc320e3d47072ae979a8a80c13ba2f
Trieu Vu <vutrieu2002@gmail.com>
2023-07-25 16:36:36 +0700

    Spring Security: create ApplicationConfig 'impl UserDetailService'

--- src/main/java/com/example/springsecurity/config/ApplicationConfig.java
+++ src/main/java/com/example/springsecurity/config/ApplicationConfig.java
@@ -1,2 +1,21 @@
-package com.example.springsecurity.config;public class ApplicationConfig {
+package com.example.springsecurity.config;
+
+import com.example.springsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(()->new UsernameNotFoundException("user not found"));
+    }
 }
