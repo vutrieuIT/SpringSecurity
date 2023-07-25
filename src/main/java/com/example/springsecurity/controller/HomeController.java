715c7cde2873e8954bbe8893c9ba97e388f93e9c
Trieu Vu <vutrieu2002@gmail.com>
2023-07-25 14:07:54 +0700

    Spring Security: basic config security

---
+++ src/main/java/com/example/springsecurity/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.springsecurity.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/home")
+    public ResponseEntity<String> home(){
+        return ResponseEntity.ok("home: everybody can access");
+    }
+}
---
+++ src/main/java/com/example/springsecurity/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.springsecurity.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/info")
+    public ResponseEntity<String> getInfo(){
+        return ResponseEntity.ok("role user: get user info");
+    }
+}
