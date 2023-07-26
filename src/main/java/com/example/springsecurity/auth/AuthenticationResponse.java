560aedec2ea37118b8f73d313b5815b371d5cb81
Trieu Vu <vutrieu2002@gmail.com>
2023-07-26 09:29:54 +0700

    Spring Security: create pkg auth 'auth user, response token'

--- src/main/java/com/example/springsecurity/auth/AuthenticationResponse.java
+++ src/main/java/com/example/springsecurity/auth/AuthenticationResponse.java
@@ -1,2 +1,15 @@
-package com.example.springsecurity.auth;public class AuthenticationResponse {
+package com.example.springsecurity.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
 }
--- src/main/java/com/example/springsecurity/auth/RegisterRequest.java
+++ src/main/java/com/example/springsecurity/auth/RegisterRequest.java
@@ -1,2 +1,18 @@
-package com.example.springsecurity.auth;public class RegisterRequest {
+package com.example.springsecurity.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String fullName;
+
+    private String email;
+
+    private String password;
 }
