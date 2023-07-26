c9d9a3d89be58c87190446de45f4c650905e9a73
Trieu Vu <vutrieu2002@gmail.com>
2023-07-26 09:37:41 +0700

    Spring Security: add @CrossOrigin 'accept req from any origin'

--- src/main/java/com/example/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/example/springsecurity/SpringSecurityApplication.java
@@ -2,7 +2,9 @@ package com.example.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @SpringBootApplication
 public class SpringSecurityApplication {
 
